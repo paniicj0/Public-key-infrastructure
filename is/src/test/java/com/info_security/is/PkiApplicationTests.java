e24e97c0241008c5acae7fc2fea00074ee11d2ae
Jovana Panic <panic.sv64.2022@uns.ac.rs>
2025-09-01 20:19:21 +0200

    lol

---
+++ is/src/test/java/com/info_security/is/PkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info_security.is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
